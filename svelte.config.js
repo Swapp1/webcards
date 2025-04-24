ceb80f35733e73bf5c23c2b08df7918df5f4e521
Swapp1 <dev@swappapps.com>
2025-04-24 12:57:09 +0200

    svelte config edit

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,21 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html', // permet le mode SPA
-			strict: false,          // évite l'erreur de routes dynamiques
-		}),
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
 		alias: {
 			"@/*": "./path/to/lib/*",
 		},
-		paths: {
-			base: '',
-		}
 	}
 };
 
