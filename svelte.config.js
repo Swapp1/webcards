03be676c4f18c6ad73a0cc1468c760f566de1ae2
Swapp1 <dev@swappapps.com>
2025-04-24 13:12:33 +0200

    fix: dynamic routes with adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import vercel from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: vercel(),
-		alias: {
-			"@/*": "src/lib/*"
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: vercel(),
+    alias: {
+      "@/*": "src/lib/*"
+    }
+  }
 };
 
 export default config;
