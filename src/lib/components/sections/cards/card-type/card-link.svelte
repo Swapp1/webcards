540616a8bf79e22dd9b135b59ea759167b678217
Swapp1 <dev@swappapps.com>
2025-04-24 16:45:41 +0200

    optimization + design changes

--- src/lib/components/sections/cards/card-type/card-link.svelte
+++ src/lib/components/sections/cards/card-type/card-link.svelte
@@ -30,109 +30,47 @@
 	const [light, dark] = getIcons(src);
 </script>
 
-{#if validLink}
-	<a
-		href={`${validLink}`}
-		target="_blank"
+<a
+	href={validLink || undefined}
+	target={validLink ? "_blank" : undefined}
+	class={cn(
+		'animate-touch flex items-center gap-[1rem] rounded-lg px-4 py-3 md:px-[1.375rem] md:py-[1.125rem]',
+		isColor ? 'bg-white/20' : 'bg-white dark:bg-[#1B1B1B]'
+	)}
+>
+	<img
+		src={light}
 		class={cn(
-			'animate-touch flex items-center gap-[1rem]  rounded-lg px-4  py-3 md:px-[1.375rem] md:py-[1.125rem]',
-			isColor ? 'bg-white/20' : 'bg-white dark:bg-[#1B1B1B]'
+			'h-[48px] w-[48px] md:h-[56px] md:w-[56px]',
+			!isColor && 'dark:hidden'
 		)}
-	>
-		{#if isColor}
-			<img
-				src={light}
-				class="h-[48px] w-[48px] md:h-[56px] md:w-[56px]"
-				alt={src}
-				width="48"
-				height="48"
-			/>
-		{:else}
-			<img
-				src={light}
-				class="h-[48px] w-[48px] dark:hidden md:h-[56px] md:w-[56px]"
-				alt={src}
-				width="48"
-				height="48"
-			/>
-			<img
-				src={dark}
-				class="hidden h-[48px] w-[48px] dark:block md:h-[56px] md:w-[56px]"
-				alt={src}
-				width="48"
-				height="48"
-			/>
-		{/if}
-		<div
-			class={cn(
-				'flex flex-col gap-[0.375rem] ',
-				isColor ? 'text-white' : 'text-black dark:text-white'
-			)}
-		>
-			<h2 class="text-[1.06rem] leading-4 md:text-xl md:leading-5">
-				{src}
-			</h2>
-			<h3
-				class={cn(
-					'text-[0.75rem] font-light leading-[0.75rem] md:text-sm md:leading-[0.875rem] ',
-					isColor ? 'text-white/70' : 'text-black/70 dark:text-white/70'
-				)}
-			>
-				{haveAt}{detail}
-			</h3>
-		</div>
-	</a>
-{/if}
-
-{#if !validLink}
+		alt={src}
+		width="48"
+		height="48"
+	/>
+	{#if !isColor}
+		<img
+			src={dark}
+			class="hidden h-[48px] w-[48px] dark:block md:h-[56px] md:w-[56px]"
+			alt={src}
+			width="48"
+			height="48"
+		/>
+	{/if}
 	<div
 		class={cn(
-			'animate-touch flex items-center gap-[1.1875rem]  rounded-lg px-4 py-3 md:px-[1.375rem] md:py-[1.125rem]',
-			isColor ? 'bg-white/20' : 'bg-white dark:bg-[#1B1B1B]'
+			'flex flex-col gap-[0.375rem]',
+			isColor ? 'text-white' : 'text-black dark:text-white'
 		)}
 	>
-		{#if isColor}
-			<img
-				src={light}
-				class="h-[48px] w-[48px] md:h-[56px] md:w-[56px]"
-				alt={src}
-				width="48"
-				height="48"
-			/>
-		{:else}
-			<img
-				src={light}
-				class="h-[48px] w-[48px] dark:hidden md:h-[56px] md:w-[56px]"
-				alt={src}
-				width="48"
-				height="48"
-			/>
-			<img
-				src={dark}
-				class="hidden h-[48px] w-[48px] dark:block md:h-[56px] md:w-[56px]"
-				alt={src}
-				width="48"
-				height="48"
-			/>
-		{/if}
-
-		<div
+		<h2 class="text-[1.06rem] leading-4 md:text-xl md:leading-5">{src}</h2>
+		<h3
 			class={cn(
-				'flex flex-col gap-[0.375rem] ',
-				isColor ? 'text-white' : 'text-black dark:text-white'
+				'text-[0.75rem] font-light leading-[0.75rem] md:text-sm md:leading-[0.875rem]',
+				isColor ? 'text-white/70' : 'text-black/70 dark:text-white/70'
 			)}
 		>
-			<h2 class="text-[1.06rem] leading-4 md:text-xl md:leading-5">
-				{src}
-			</h2>
-			<h3
-				class={cn(
-					'text-[0.75rem] font-light leading-[0.75rem] md:text-sm md:leading-[0.875rem] ',
-					isColor ? 'text-white/70' : 'text-black/70 dark:text-white/70'
-				)}
-			>
-				{haveAt}{detail}
-			</h3>
-		</div>
+			{haveAt}{detail}
+		</h3>
 	</div>
-{/if}
+</a>
